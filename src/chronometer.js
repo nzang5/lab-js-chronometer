9af997d8ddf1184144b00d5b0647fffeef647a35
Nash Zangio <nash.c.zangio@gmail.com>
2022-07-28 15:26:24 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+   this.currentTime = 0;
+   this.intervalId = null;       //iteration 1
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+   this.intervalId = setInterval(()=> {
+    
+    this.currentTime++;
+
+    if(printTimeCallback){
+      return printTimeCallback();
+    }
+    
+   }, 1000) // iteration 2
   }
 
   getMinutes() {
-    // ... your code goes here
+    let totalTime = Math.floor(this.currentTime / 60);
+    return totalTime;                                 // iteration 3
   }
 
   getSeconds() {
-    // ... your code goes here
+    let totalSeconds = (this.currentTime % 60);
+    return totalSeconds;   // iteration 4
   }
 
+  // getCentiseconds() {
+  //   let centiSeconds = (this.getSeconds / 100);
+  //   return centiSeconds;
+  //   }
+
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value >= 10){
+    return `${value}`;
+    }
+
+   else if(value < 10 && value > 0){
+       return `0${value}`;                         // iteration 4
+    }
+    else{
+      return `00`
+    }
   }
 
   stop() {
-    // ... your code goes here
+   clearInterval(this.intervalId);                // iteration 5
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0;                     // iteration 6
   }
 
   split() {
-    // ... your code goes here
+   let minutes = this.computeTwoDigitNumber(this.getMinutes());
+   let seconds = this.computeTwoDigitNumber(this.getSeconds());
+ // let centiSeconds = this.computeTwoDigitNumber(this.getcentiSeconds());
+   
+    return `${minutes}:${seconds}` 
+  
   }
+ 
+
 }
